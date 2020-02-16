50f42358a7bf52a0c2cbbfa6ec660af4babc263f
aboumaza <ad.boumaza@yahoo.fr>
2020-02-16 20:04:32 +0100

    he

--- src/test/java/dz/agenceadam/locationvoiture/Driver.java
+++ src/test/java/dz/agenceadam/locationvoiture/Driver.java
@@ -17,41 +17,6 @@ import org.joda.time.DateTime;
 
 public class Driver {
 
-	public static void main(String[] args) throws ParseException {
-	    
-		Map<Integer, Date> tab = generateDaysOfMonthAndYears(2018, 9);
-		System.out.println(tab);
-		
-
-	}
 	
-	public static Map<Integer, Date> generateDaysOfMonthAndYears(int year,int month) throws ParseException
-	{
-		Map<Integer,Date> tabDays = new HashMap<>();
-		YearMonth yearMonthObject = YearMonth.of(year, month);
-		int daysInMonth = yearMonthObject.lengthOfMonth(); //28 
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
-		for(int x=1;x<=daysInMonth;x++)
-		{
-			LocalDate ld = LocalDate.of( year , month , x );
-			tabDays.put(x, sdf.parse(ld.toString()));
-		}
-		
-		return tabDays;
-	}
-	
-	private static List<Integer> generateDaysOfMonthAndYearsE(int year,int month)
-	{
-		List<Integer> tabDays = new ArrayList<>();
-		YearMonth yearMonthObject = YearMonth.of(year, month);
-		int daysInMonth = yearMonthObject.lengthOfMonth(); //28 
-		for(int x=1;x<=daysInMonth;x++)
-		{
-			tabDays.add(x);
-		}
-		
-		return tabDays;
-	}
 }
 
